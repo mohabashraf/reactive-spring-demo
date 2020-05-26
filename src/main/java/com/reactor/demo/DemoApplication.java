c6eb24a2b1ccae5d265afaf701db50cafd7e6285
mohabashraf <mohabashraf466@gmail.com>
2020-05-26 13:59:05 +0200

    add reactive endpoint

--- src/main/java/com/reactor/demo/DemoApplication.java
+++ src/main/java/com/reactor/demo/DemoApplication.java
@@ -9,20 +9,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routes(GreetingService gs){
+		return route()
+				.GET("greeting/name", serverRequest -> ok().body(gs.greet(), String.class))
+				.build();
+	}
+}
+
+
+@Service
+class GreetingService {
+	Mono<String> greet(){
+		return Mono.just("Hi there");
 	}
 }
 
