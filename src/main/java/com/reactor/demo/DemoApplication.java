f164b38a9761cc077793d4ca410ce2f6b72e109f
mohabashraf <mohabashraf466@gmail.com>
2020-05-26 20:32:41 +0200

    add elastic scheduler

--- src/main/java/com/reactor/demo/DemoApplication.java
+++ src/main/java/com/reactor/demo/DemoApplication.java
@@ -19,6 +19,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.util.UUID;
@@ -50,7 +52,7 @@ class GreetingService {
 
 	Flux<String> greetMany(){
 		return Flux.fromStream(Stream.generate(() -> "Ahmed"))
-				.delayElements(Duration.ofSeconds(1));
+				.delayElements(Duration.ofSeconds(1)).subscribeOn(Schedulers.elastic());
 	}
 	Mono<String> greetOnce(){
 		return Mono.just("Hi there");
